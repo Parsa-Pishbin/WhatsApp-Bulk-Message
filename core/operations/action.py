d1d17ce25554967d95032525786454ecc9dd62dd
Parsa <parsapishbin1382@gmail.com>
2025-04-05 01:26:18 +0330

    feat(core): add send_by_button to actionmixin
    send_by_button method send written by clicking the send button

--- core/operations/action.py
+++ core/operations/action.py
@@ -1,3 +1,17 @@
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from .operation import BaseOperation
 
-class ActionMixin(BaseOperation): ...
+class ActionMixin(BaseOperation):
+    def send_by_button(self):
+        try:
+            send_button = self.browser.find_element(By.XPATH, self.configurations['send_key'])
+            send_button.click()
+        except (NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException) as exception:
+            print(exception)
+            return False
+        
+        return True
+    
