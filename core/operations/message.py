8d8cb1e18388ffa6996a5d14e12943f02d098d04
Parsa <parsapishbin1382@gmail.com>
2025-04-05 01:16:28 +0330

    feat(core): add write_text_message to messagemixin
    write text messages in input box, line by line

--- core/operations/message.py
+++ core/operations/message.py
@@ -1,3 +1,25 @@
+from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
 from .operation import BaseOperation
 
-class MessageMixin(BaseOperation): ...
+class MessageMixin(BaseOperation):
+    def write_text_message(self, message:str):
+        '''
+        write text message in input box
+        '''
+
+        try:
+            text_input = self.browser.find_element(By.XPATH, self.configurations['text_input'])
+        
+            text_input.click()
+            text_input.clear()
+            for line in message.splitlines():
+                text_input.send_keys(line)
+                text_input.send_keys(Keys.SHIFT ,Keys.RETURN)
+        except (NoSuchElementException, ElementNotInteractableException) as exception:
+            print(exception)
+            return False
+        
+        return True
