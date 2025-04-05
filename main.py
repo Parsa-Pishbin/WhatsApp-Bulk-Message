9d9edc82de287cc1ceddf091c6c2345c9cefdd0e
Parsa <parsapishbin1382@gmail.com>
2025-04-05 19:39:10 +0330

    chore: use whatsappbot in main
    Configurations WhatsAppBot and use it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import getcwd
 
+from core.core import WhatsAppBot
 from tools.data import read_message_file, read_numbers_file, verify_numbers
 
 # import read_config
@@ -18,6 +19,14 @@ if not NUMBERS:
     
 # read config
 # send data to core
+
+wa_bot = WhatsAppBot(BASE_DIR)
+
+for number in NUMBERS:
+    wa_bot.go_to_user_by_phone(number)
+    wa_bot.write_text_message(MESSAGE)
+    wa_bot.send_by_enter()
+    
 #   do operations
 
 # show end message
