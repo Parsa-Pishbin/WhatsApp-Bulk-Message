9b031f5f909ad3c6837e2d2d9035fe2d6f52f00c
Parsa <m.p.pishbin@gmail.com>
2025-04-06 01:06:23 +0330

    feat(core): build logmanager in log.py
    LogManager handel log process for bots

---
+++ core/log.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+class LogManager:
+    def __init__(self, bot, log_file_path):
+        self.log_file_path = log_file_path
+        self.log_text = '{}       {}       {}       ' + str(bot) + '\n'
+
+    def save(self, number:str, status:bool):
+        time = datetime.now()
+        status = 'Sent' if status else 'X   '
+
+        with open(self.log_file_path, 'a') as log_file:
+            log_file.write(self.log_text.format(status, number, time))
