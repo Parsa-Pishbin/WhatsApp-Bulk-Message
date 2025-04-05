d7aa0713ec9a6a42770b58c457a948c35f35dc4e
Parsa <m.p.pishbin@gmail.com>
2025-04-06 01:47:34 +0330

    chore: adapt main with whatsappbot log

--- main.py
+++ main.py
@@ -23,8 +23,7 @@ general_configs = configs.read(join_path(BASE_DIR, 'general_config'))
 
 wa_bot = WhatsAppBot(
     BASE_DIR,
-    extra_delay = int(general_configs['extra_delay']),
-    headless = True if general_configs['activity_invisible'] == 'True' else False
+    general_configs,
     )
 
 wa_bot.login()
@@ -33,7 +32,14 @@ for number in NUMBERS:
     wa_bot.go_to_user_by_phone(number)
     wa_bot.wait_for_load()
     wa_bot.write_text_message(MESSAGE)
-    wa_bot.send_by_enter()
+    wa_bot.log_manager.save(number, wa_bot.send_by_enter())
+    
+    # if wa_bot.send_by_enter():
+    #     wa_bot.log_manager.save(number, True)
+    # else:
+    #     wa_bot.log_manager.save(number, False)
+
+
     
 #   do operations
 
