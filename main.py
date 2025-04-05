2e63e527708b00565648669eb3f4e70a979aa93c
Parsa <parsapishbin1382@gmail.com>
2025-04-05 13:01:33 +0330

    chore: read numbers file and message file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-# import from core
+from os import getcwd
+
+from tools.data import read_message_file, read_numbers_file, verify_numbers
+
 # import read_config
 
-# read config
+BASE_DIR = getcwd()
+MESSAGE = read_message_file(BASE_DIR)
+NUMBERS = verify_numbers(read_numbers_file(BASE_DIR))
 
 # send data to core
 #   do operations
