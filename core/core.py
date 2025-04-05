a9d66e73c2200d5307e14460dc3f040cc7fb73fa
Parsa <m.p.pishbin@gmail.com>
2025-04-06 00:14:05 +0330

    feat(core): add extra_delay arg to whatsappbot

--- core/core.py
+++ core/core.py
@@ -8,14 +8,15 @@ from .operations import action, message, data
 
 
 class WhatsAppBot(action.ActionMixin, message.MessageMixin, data.DataMixin):
-    def __init__(self, BASE_DIR, headless=False):
+    def __init__(self, BASE_DIR, extra_delay=0, headless=False):
         '''
         WhatsAppBot is a agent between us and whatsapp web
         '''
         
         config_path = path.join(BASE_DIR, 'core\core_config')
         self.configurations = configs.read(config_path)
-        
+        self.extra_delay = extra_delay
+
         service = Service(executable_path=self.configurations['web_driver_path'])
         options = Options()
         if headless:
