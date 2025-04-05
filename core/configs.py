db0f5663fab1897b73161533cb4a02fca39be58f
Parsa <parsapishbin1382@gmail.com>
2025-04-05 23:00:04 +0330

    chore(core): build configs.read module

--- core/configs.py
+++ core/configs.py
@@ -1,6 +1,14 @@
-# import nessecary module
+from configparser import ConfigParser
 
 
 def read(path:str) -> dict : 
     '''read config file and return data in dict'''
-    
+
+    configs = {}
+    config_parser = ConfigParser()
+    config_parser.read(path)
+
+    for section in config_parser:
+        configs |= dict(config_parser[section])
+
+    return configs
