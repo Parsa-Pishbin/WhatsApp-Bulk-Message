9309406abeedf63ea56f6805c8441f69bc84274c
Parsa <m.p.pishbin@gmail.com>
2025-04-06 21:51:38 +0330

    feat(core.configs): use ConfigFileNotFoundError

--- core/configs.py
+++ core/configs.py
@@ -1,13 +1,18 @@
 from configparser import ConfigParser
 
+from .exceptions import ConfigFileNotFoundError
+
 
 def read(path:str) -> dict : 
     '''read config file and return data in dict'''
 
     configs = {}
     config_parser = ConfigParser()
-    config_parser.read(path)
+    read_files_list = config_parser.read(path)
 
+    if not read_files_list:
+        raise ConfigFileNotFoundError
+    
     for section in config_parser:
         configs |= dict(config_parser[section])
 
