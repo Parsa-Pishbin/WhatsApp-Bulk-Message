bf7d4f7cf384cd40a7328dd2b6a4bafd80b51da3
Parsa <m.p.pishbin@gmail.com>
2025-04-06 20:57:10 +0330

    feat(tools): use core.exceptions in tools.data

--- tools/data.py
+++ tools/data.py
@@ -1,5 +1,12 @@
 from re import fullmatch
 
+from core.exceptions import (MessageFileNotFoundError, 
+                             NumbersFileNotFoundError, 
+                             EmptyMessageFileError, 
+                             EmptyNumbersFileError, 
+                             NoApprovedNumbersError)
+
+
 def read_message_file(path:str) -> str:
     '''read message.txt and return the content'''
     
@@ -7,11 +14,13 @@ def read_message_file(path:str) -> str:
         with open(path, encoding="utf8") as file:
             message = file.read()
     except FileNotFoundError as exception:
-        print(exception)
-        return
+        raise MessageFileNotFoundError
     
     if message:
         return message
+
+    raise EmptyMessageFileError
+    
 
 
 def read_numbers_file(path:str) -> list[str]:
@@ -19,16 +28,20 @@ def read_numbers_file(path:str) -> list[str]:
         with open(path, encoding="utf8") as file:
             numbers = file.readlines()
     except FileNotFoundError as exception:
-        print(exception)
-        return
+        raise NumbersFileNotFoundError
 
-    return numbers
+    if numbers:
+        return numbers
+
+    raise EmptyNumbersFileError
 
 
 def verify_numbers(numbers_list:list[str]) -> list[str]:
-    if numbers_list is None:
-        return
     cleaned_numbers_list = map(lambda number: number.replace(' ', '').replace('\n', ''), numbers_list)
     filtered_numbers = filter(lambda number: True if fullmatch('^(\+98|0)?9\d{9}$', number) else False, cleaned_numbers_list)
     approved_numbers = list(map(lambda number: '+98' + number if number.startswith('9') else number.replace('0', '+98', 1), filtered_numbers))
-    return approved_numbers
+    
+    if approved_numbers:
+        return approved_numbers
+
+    raise NoApprovedNumbersError
