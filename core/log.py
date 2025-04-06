e8c880b2506775ae208f43a25c32ed7fc0749f37
Parsa <m.p.pishbin@gmail.com>
2025-04-06 21:46:58 +0330

    feat(core.log): use BadLogPathError exception
    use BadLogPathError in LogManager

--- core/log.py
+++ core/log.py
@@ -1,7 +1,13 @@
 from datetime import datetime
+from os.path import exists as is_path_exist, isdir as is_path_dir
+
+from .exceptions import BadLogPathError
 
 class LogManager:
     def __init__(self, bot, log_file_path, terminal_log=False):
+        if not is_path_exist(log_file_path) or is_path_dir(log_file_path):
+            raise BadLogPathError
+        
         self.log_file_path = log_file_path
         self.log_text = '{}       {}       {}       ' + str(bot) + '\n'
         self.terminal_log = terminal_log
